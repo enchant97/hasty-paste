5090678900729baa098756d3ec4cb5cca0c7e85f
Leo Spratt <enchant97@users.noreply.github.com>
2022-07-28 20:46:27 +0100

    fix health check

--- scripts/health-check.sh
+++ scripts/health-check.sh
@@ -2,9 +2,9 @@
 
 if [ -n "$CERT_FILE" ] && [ -n "$KEY_FILE" ]
 then
-    args="https://127.0.0.1:$PORT/is-healthy --allow-unverified"
+    args="https://127.0.0.1:$PORT/api/is-healthy --allow-unverified"
 else
-    args="http://127.0.0.1:$PORT/is-healthy"
+    args="http://127.0.0.1:$PORT/api/is-healthy"
 fi
 
 python -m web_health_checker $args
