f822bb48d1c64681c85a05ca0cde7ab803724139
Leo Spratt <enchant97@users.noreply.github.com>
2022-07-31 15:08:19 +0100

    add version number

--- paste_bin/main.py
+++ paste_bin/main.py
@@ -4,16 +4,18 @@ from quart import Quart
 from quart_schema import QuartSchema
 from web_health_checker.contrib import quart as health_check
 
-from . import views
+from . import __version__, views
 from .config import get_settings
 
 logger = logging.getLogger("paste_bin")
+app_version = ".".join(__version__.split(".")[0:2])
 app = Quart(__name__)
 quart_schema = QuartSchema(
     openapi_path="/api/openapi.json",
     swagger_ui_path="/api/docs",
     redoc_ui_path="/api/redocs",
     title="Hasty Paste",
+    version=app_version,
 )
 
 
@@ -21,7 +23,7 @@ def _reset_app():
     """
     reset the internals for use only when running unit tests
     """
-    global app, quart_schema
+    global app
     app = Quart(__name__)
     get_settings.cache_clear()
 
@@ -35,6 +37,7 @@ def create_app():
     settings.PASTE_ROOT.mkdir(parents=True, exist_ok=True)
 
     app.config["MAX_CONTENT_LENGTH"] = settings.MAX_BODY_SIZE
+    app.config["__version__"] = app_version
 
     app.register_blueprint(views.front_end)
     app.register_blueprint(health_check.blueprint, url_prefix="/api")
