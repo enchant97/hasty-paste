cda8a5b28ad517df44fda087cdb5ec97b8f1e1c6
Leo Spratt <enchant97@users.noreply.github.com>
2022-09-30 18:57:07 +0100

    hide static routes from api docs

--- paste_bin/views/extra_static.py
+++ paste_bin/views/extra_static.py
@@ -1,4 +1,5 @@
 from quart import Blueprint, abort, send_file
+from quart_schema import hide_route
 
 from ..config import get_settings
 
@@ -6,6 +7,7 @@ blueprint = Blueprint("extra_static", __name__, url_prefix="/static")
 
 
 @blueprint.get("/brand.css")
+@hide_route
 async def get_brand_css():
     path = get_settings().BRANDING.CSS_FILE
     if not path:
@@ -14,6 +16,7 @@ async def get_brand_css():
 
 
 @blueprint.get("/brand-icon")
+@hide_route
 async def get_brand_icon():
     path = get_settings().BRANDING.ICON
     if not path:
@@ -22,6 +25,7 @@ async def get_brand_icon():
 
 
 @blueprint.get("/brand-favicon")
+@hide_route
 async def get_brand_favicon():
     path = get_settings().BRANDING.FAVICON
     if not path:
