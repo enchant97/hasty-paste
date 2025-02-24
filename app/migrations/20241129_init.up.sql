d9010b8f1be8036225ec73e7e1ffffe5017e1bd7
Leo Spratt <enchant97@users.noreply.github.com>
2025-02-24 17:38:45 +0000

    compact db migrations

--- app/migrations/20241129_init.up.sql
+++ app/migrations/20241129_init.up.sql
@@ -7,6 +7,19 @@ CREATE TABLE
   );
 
 CREATE TABLE
+  oidc_users (
+    id INTEGER PRIMARY KEY,
+    user_id UUID NOT NULL,
+    client_id TEXT NOT NULL,
+    user_sub TEXT NOT NULL,
+    UNIQUE (user_id, client_id),
+    UNIQUE (client_id, user_sub),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+  );
+
+CREATE UNIQUE INDEX oidc_client_sub_idx ON oidc_users (client_id, user_sub);
+
+CREATE TABLE
   pastes (
     id UUID PRIMARY KEY,
     owner_id UUID NOT NULL,
